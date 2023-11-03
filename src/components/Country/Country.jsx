c18f24ce48b5d5308c3e8849c348cde3784d87b2
mazharulislam-12 <505mazharulislam@gmail.com>
2023-11-03 07:09:33 -0700

    adding popolutain, areae, code , dynamic button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, capital, population, area, cca3, independent  } = country;
+
+    const [visited, setvisited] = useState(false)
+
+    const handlevistied = ()=>{
+        setvisited(!visited)
+    }
+
+
     return (
         <div className='country'>
-           <h3>Name: {name?.common} </h3> 
            <img src= {flags.png} alt="" />
+           <h3>Name: {name?.common} </h3> 
+           <p>Area: {area} </p>
+           <p>Capital: {capital} </p>
+           <p>Population : {population} </p>
+           <p><small>Code: {cca3} </small></p>
+           <button onClick={handlevistied}>{visited ? 'Visited': 'Going'}</button>
+           {visited ? "I have visited this country" : "I want to visited"}
+
         </div>
     );
 };
--- src/components/Countryes/Countries.jsx
+++ src/components/Countryes/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
 const Countries = () => {
 
@@ -16,9 +17,13 @@ const Countries = () => {
             
             <h3>countries : {countries.length} </h3>
 
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+            <div className="country-container">
+                {
+                    countries.map(country => <Country 
+                        key={country.cca3}
+                        country={country}></Country>)
+                }
+            </div>
 
 
         </div>
