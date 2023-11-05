9019738f206d41b7694f6ac8e49d561ded33c7b0
mazharulislam-12 <505mazharulislam@gmail.com>
2023-11-04 21:38:57 -0700

    mark as visited country , and and going button click to color change

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country, handlevistiedCountry}) => {
     const {name, flags, capital, population, area, cca3, independent  } = country;
 
     const [visited, setvisited] = useState(false)
@@ -9,15 +9,20 @@ const Country = ({country}) => {
         setvisited(!visited)
     }
 
+    const passWithParams = () =>handlevistiedCountry(country)
+    
+
 
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}` }>
            <img src= {flags.png} alt="" />
-           <h3>Name: {name?.common} </h3> 
+           <h2 className='text-xl' style={{color: visited ? 'purple': 'white'}}>Name: {name?.common} </h2> 
            <p>Area: {area} </p>
            <p>Capital: {capital} </p>
            <p>Population : {population} </p>
            <p><small>Code: {cca3} </small></p>
+           <button onClick={()=>handlevistiedCountry(country)}>Mark Visited</button>
+           <br />
            <button onClick={handlevistied}>{visited ? 'Visited': 'Going'}</button>
            {visited ? "I have visited this country" : "I want to visited"}
 
@@ -26,3 +31,4 @@ const Country = ({country}) => {
 };
 
 export default Country;
+
--- src/components/Countryes/Countries.jsx
+++ src/components/Countryes/Countries.jsx
@@ -6,21 +6,35 @@ import './Countries.css';
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setvisitedCountries] = useState([]);
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
             .then(res =>res.json())
             .then(data => setCountries(data))
     },[])
 
+    const handlevistiedCountry = country =>{
+        console.log('add this to your visited country')
+        console.log(country)
+    }
+
     return (
         <div>
             
             <h3>countries : {countries.length} </h3>
+            <div>
+                <h5>Visited countries</h5>
+                <ul>
+                    
+                </ul>
+            </div>
 
             <div className="country-container">
                 {
                     countries.map(country => <Country 
                         key={country.cca3}
+                        handlevistiedCountry={handlevistiedCountry}
                         country={country}></Country>)
                 }
             </div>
